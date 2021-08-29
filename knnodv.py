0bdf3b41d639b8ee799df0df648a7700a1b7029f
yunusakkaya <57574383+yunusakkaya@users.noreply.github.com>
2021-08-29 10:57:05 +0300

    Add files via upload

---
+++ knnodv.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data=pd.read_csv(r"C:\Users\yunus\Desktop\udemy\column-2C-weka.csv")
+
+color_list = ['red' if i=='Abnormal' else 'green' for i in data.loc[:,'class']]
+pd.plotting.scatter_matrix(data.loc[:, data.columns != 'class'],
+                                       c=color_list,
+                                       figsize= [15,15],
+                                       diagonal='hist',
+                                       alpha=0.5,
+                                       s = 200,
+                                       marker = '*',
+                                       edgecolor= "black")
+plt.show()
+
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors = 3)
+x,y = data.loc[:,data.columns != 'class'], data.loc[:,'class']
+knn.fit(x,y)
+prediction = knn.predict(x)
+print('Prediction: {}'.format(prediction))
+
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size = 0.3,random_state = 1)
+knn = KNeighborsClassifier(n_neighbors = 3)
+x,y = data.loc[:,data.columns != 'class'], data.loc[:,'class']
+knn.fit(x_train,y_train)
+prediction = knn.predict(x_test)
+
+print('With KNN (K=3) accuracy is: ',knn.score(x_test,y_test))
